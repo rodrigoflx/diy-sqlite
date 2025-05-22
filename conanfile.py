1e9ee6cfed9ff648dd0ac9a9a6fd00d1ba61343d
rodff <rod.forno@gmail.com>
2025-05-23 00:00:49 +0200

    Corrected C++ version to C+17 and added tl/expected as an dependency

--- conanfile.py
+++ conanfile.py
@@ -1,11 +1,12 @@
 from conan import ConanFile
-
+from conan.tools.cmake import cmake_layout
 
 class Recipe(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
     generators = "CMakeToolchain", "CMakeDeps", "VirtualRunEnv"
 
     def layout(self):
+        cmake_layout(self)
         self.folders.generators = "conan"
 
     def requirements(self):
